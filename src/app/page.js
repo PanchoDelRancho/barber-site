5ef3d085610bb7bfc7caf2bac673a2f25c4e8006
Roger Martinez <mroger612@gmail.com>
2023-04-18 18:18:33 -0500

    remnamed homepage folder to components and deleted page.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@chakra-ui/react"
 export default function Home() {
   return (
       <div>
-        <Button as="a" href="./homepage">home</Button>
+      Hello world! Home
       </div>
   )
 }
